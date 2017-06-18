422a6f93585ce6e656ab8c1cc2b8a24fd8c7f268
Jacek <jacek.paszek@onet.pl>
2017-06-18 20:17:29 +0200

    code formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-function getTriangleArea(a, h) {                //function for triangle area calculation
+function getTriangleArea(a, h) {
   return a > 0 && h > 0 ? a * h / 2 : 'Nieprawidłowe dane';
-  };
+};
+
 console.log('triangle1Area = ' + getTriangleArea(14, 23));
 console.log('triangle2Area = ' + getTriangleArea(0, 12));
 console.log('triangle3Area = ' + getTriangleArea(19, 15));
